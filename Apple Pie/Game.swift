822806b6b9b7c9fb3d9111bd9f25afbca5a6d57c
Horacio Guzmán Parra <hgp691@gmail.com>
2022-04-24 15:26:51 -0500

    Fix One

--- Apple Pie/Game.swift
+++ Apple Pie/Game.swift
@@ -8,16 +8,27 @@
 import Foundation
 import UIKit
 
-struct Game {
+class Game {
+    
+    // MARK: - Variables
     var incorrectMovesRemain : Int
-    var word : String
-    var guessedLetters : [Character] 
-    //mutating hace que podamos cambiar el valor predeterminado de una propiedad
-    mutating func charactersSelected(letraEscogida : Character) {
+    let word : String
+    var guessedLetters : [Character]
+    
+    // MARK: - Methods
+    init(incorrectMovesRemain: Int = 7,
+         word: String) {
+        self.incorrectMovesRemain = incorrectMovesRemain
+        self.word = word
+        self.guessedLetters = []
+    }
+    
+    ///mutating hace que podamos cambiar el valor predeterminado de una propiedad
+    ///- Parameter letraEscogida: Letra que fue escogida por un jugador
+    func charactersSelected(letraEscogida : Character) {
         guessedLetters.append(letraEscogida)
         if !word.contains(letraEscogida) {
             incorrectMovesRemain -= 1
         }
     }
-    
 }
--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -8,40 +8,70 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Outlets
     @IBOutlet var ImagenArbol: UIImageView!
     @IBOutlet var CorrectWordLabel: UILabel!
     @IBOutlet var ScoreLabel: UILabel!
     @IBOutlet var LetterButtons: [UIButton]!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    @IBAction func BotonesConLetras(_ sender: UIButton) {
-        sender.isEnabled = false
-        if let letterString : String = sender.title(for: .normal) {
-            let letter = Character(letterString.lowercased())
-            currentGame.charactersSelected(letraEscogida: letter)
-            newRound()
-        }else {
-            print("Algo esta mal")
-        }
-        
-    }
     
-    
-    var currentGame : Game!
-    var listOfWords : Array<String> = ["corsario, caballero, televisor, matematicas, ingles, cosina, estufa, nevera, horno, tenedor, parque, pinguinos, leones, perros, caballos, castillo"]
+    // Constants
+    var currentGame: Game?
+    var listOfWords = [
+        "corsario",
+        "caballero",
+        "televisor",
+        "matematicas",
+        "ingles",
+        "cosina",
+        "estufa",
+        "nevera",
+        "horno",
+        "tenedor",
+        "parque",
+        "pinguinos",
+        "leones",
+        "perros",
+        "caballos",
+        "castillo"
+    ]
     let incorrectMovesAllowed = 7
     var totalOfWins = 0
     var totalOfLosses = 0
     
+    // MARK: - ViewController LifeCycle
+    // Ciclo de vida de un ViewController
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        newRound()
+    }
+    
+    // MARK: - Methods
+    @IBAction func BotonesConLetras(_ sender: UIButton) {
+        
+        sender.isEnabled = false
+        
+        guard let letterstring = sender.titleLabel?.text?.lowercased(),
+              let currentGame = currentGame else {
+            return
+        }
+        
+        let letter = Character(letterstring)
+        currentGame.charactersSelected(letraEscogida: letter)
+    }
+    
     func newRound (){
         let newWord = listOfWords.removeFirst()
-        currentGame = Game(incorrectMovesRemain: incorrectMovesAllowed, word: newWord, guessedLetters: [])
+        currentGame = Game(incorrectMovesRemain: incorrectMovesAllowed,
+                           word: newWord)
         updateUI()
-        
     }
+    
     func updateUI() {
+        guard let currentGame = currentGame else {
+            return
+        }
         ScoreLabel.text = "Triunfos : \(totalOfWins), derrotas : \(totalOfLosses)"
         ImagenArbol.image = UIImage(named: "Arbol \(currentGame.incorrectMovesRemain)")
     }
